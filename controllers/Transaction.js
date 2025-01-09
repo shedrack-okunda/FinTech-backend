af3e90ae6809b0e06bf515480281d2feaab8c0bf
Shedrack Okunda <okundashedrack@gmail.com>
2025-01-09 14:54:19 +0300

    commit

--- controllers/Transaction.js
+++ controllers/Transaction.js
@@ -1,4 +1,30 @@
 import Transaction from "../models/Transaction.js";
+
+export const getPaginatedTransactionsByUser = async (req, res) => {
+  try {
+    const userId = req.user.id; // Ensure `req.user` is set by `verifyToken`
+    const { page = 1, limit = 5 } = req.query; // Default values if not provided
+
+    const skip = (page - 1) * limit; // Calculate documents to skip
+    const [transactions, total] = await Promise.all([
+      Transaction.find({ user: userId })
+        .select("createdAt type amount status")
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(parseInt(limit, 10)),
+      Transaction.countDocuments({ user: userId }),
+    ]);
+
+    res.status(200).json({
+      transactions,
+      totalPages: Math.ceil(total / limit),
+      currentPage: parseInt(page, 10),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving transactions.", error });
+  }
+};
+
 // Create a transaction
 export const createTransaction = async (req, res) => {
   try {
@@ -34,8 +60,11 @@ export const getTransactionsByUserId = async (req, res) => {
 // Get all transactions for a user
 export const getTransactionsByUser = async (req, res) => {
   try {
-    const { userId } = req.params;
-    const transactions = await Transaction.find({ user: userId });
+    const { userId } = req.user.id;
+    const transactions = await Transaction.find({ user: userId })
+      .select("CreatedAt type amount status")
+      .sort({ createdAt: -1 });
+
     res.status(200).json(transactions);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving transactions.", error });
--- routes/Transaction.js
+++ routes/Transaction.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import {
   createTransaction,
+  getPaginatedTransactionsByUser,
   getTransactionsByUser,
   getTransactionsByUserId,
   updateTransactionStatus,
@@ -10,9 +11,10 @@ import {
 const router = express.Router();
 
 router
-  .post("/", verifyToken, createTransaction)
-  .get("/:id", verifyToken, getTransactionsByUserId)
-  .get("/user/:userId", verifyToken, getTransactionsByUser)
-  .patch("/:id/status", verifyToken, updateTransactionStatus);
+  .post("/", createTransaction)
+  .get("/:id", getTransactionsByUserId)
+  .get("/user", getTransactionsByUser)
+  .get("/user", getPaginatedTransactionsByUser)
+  .patch("/:id/status", updateTransactionStatus);
 
 export default router;
